2de6f0fd9329a9a9b07f38bbf49e2d291451a964
tigerish009 <tigerish@gmail.com>
2021-06-29 21:12:36 +0900

    MAP - {foodIlike.map(dish => (<food name={dish.name} picture={dish.image} /> ))}

--- src/App.js
+++ src/App.js
@@ -4,22 +4,53 @@ import React from 'react';
 // 재사용 가능한 컴포넌트를 만들어 반복해서 사용할 수 있음
 // App안에 있는 Food component의 fav등 prop name을 function Food(props) argument로 받아옴
 // {fav}는 props내부의 object내부의 fav를 불러오는 것으로, props.fav와 같다 JS의 ES6기능임
-function Food({ fav }) {
+function Food({name, picture}) {
   return (
-    <h2>I like {fav}</h2>
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt=""/>
+    </div>
   )
 }
+
+const foodIlike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!!!</h1>
       {/* Food component에 fav라는 이름의 property를 생성하고, kimchi라는 prop name을 부여함(property name) */}
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="apple" />
-      <Food fav="cake" />
-      <Food fav="coffee" />
+      {/* 그냥쓰면 html, 중괄호 쓰면 JS */}
+      {/* map은 각 인자마다 function을 실행해주고 결과를 Array로 저장해준다. */}
+      {/* dish는 object이며 김치 object, 삼겹살 object등을 가지게 되고, 각 object는 안에 name을 갖고있으므로 object.name이 존재한다. */}
+      {foodIlike.map(dish => (
+        <Food name={dish.name} picture={dish.image} /> 
+      ))}
     </div>
   );
 }
