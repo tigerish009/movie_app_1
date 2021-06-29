d8eaac04619901e68d1007c64648fac651f056d4
tigerish009 <tigerish@gmail.com>
2021-06-29 17:14:34 +0900

    create JSX and input other JSX(App.js)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Potato from './Potato';
 
 function App() {
   return (
     <div>
       Hello!!!!!!
+      <Potato />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Potato from './Potato';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* component, <App />형태로 써야함, JSX라고함. JSX는 javascript안의 HTML */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
