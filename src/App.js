25c38a534d00937c49c3e5b4d2455d3a5983fa8e
tigerish009 <tigerish@gmail.com>
2021-06-30 17:51:34 +0900

    getMoives = async()=>{const movies = await axios.get(https://yts-proxy.now.sh/list_movies.json);}

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 
 class App extends React.Component {
   
@@ -7,10 +9,15 @@ class App extends React.Component {
     movies: []
   }
 
+  getMovies = async () => {
+    // axios 등으로 api를 호출할때는 속도가 느릴수 있기때문에 JS에게 시간이 걸린다고 말해줘야한다. 이것을 비동기라고 하며, async, await로 구현한다. JS에게 기다리라가 말하는 것.
+    // axios는 javascript의 fetch와 같은것으로 fetch에 layer가 감싸져있어 더욱 유용한 API호출 코드 (npm i axios) 
+    // async = 기다려!, await = 무엇을?
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000)
+    this.getMovies();
   }
   
   render(){
