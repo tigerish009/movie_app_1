890862cb0c84a68e9fe3d4fceb1d1b08c2b2748c
tigerish009 <tigerish@gmail.com>
2021-06-30 21:55:21 +0900

    change class to className, {genres.map((genre, index) => (<li key={index} className=genres__genre>{genre}</li>))}

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
-function Movie({year, title, summary, poster}) {
+function Movie({year, title, summary, poster, genres}) {
     return (
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
-            <div class="movie_data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+            <div className="movie_data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {/* map은 자동으로 2번째 인자(index)에 key값을 부여해준다  */}
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">{genre}</li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary}</p>
             </div>
         </div>
     )
@@ -20,7 +26,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
