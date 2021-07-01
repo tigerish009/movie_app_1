120c2fe5d397221925050f680c0ec3bbd2a193db
tigerish009 <tigerish@gmail.com>
2021-07-01 14:23:27 +0900

    Navigation() {<Link to=/about>} instead of <a href= />

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
-import About from "./routes/About";
 import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from './components/Navigation';
+import "./App.css";
 
 function App() {
     return (
     <HashRouter>
+        {/* Link to 가 있다면 Router 안에 넣어야 한다. */}
+        <Navigation />
         {/* 2개의 props이며, path로 가면, component를 보여준다 */}
         <Route path="/" exact={true} component={Home} />
         <Route path="/about" component={About} />
